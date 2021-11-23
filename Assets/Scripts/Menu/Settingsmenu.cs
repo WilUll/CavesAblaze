df3f12c4ac040a06496eb529d4d9e72ebab10ca0
Erikjohannesson01 <erijoh0517@skola.goteborg.se>
2021-11-23 13:53:29 +0100

    Volume Slider
    Volume slider in options

---
+++ Assets/Scripts/Menu/Settingsmenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settingsmenu : MonoBehaviour
+{
+
+    public AudioMixer audiomixer;
+
+    public void SetVolume (float volume)
+    {
+        audiomixer.SetFloat("volume", volume);
+    }
+}
