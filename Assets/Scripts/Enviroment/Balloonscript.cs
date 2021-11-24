86174d3e890826fec1173957bbc79f516f8840c9
Erikjohannesson01 <erijoh0517@skola.goteborg.se>
2021-11-24 11:38:22 +0100

    Fixed balloon

--- Assets/Scripts/Enviroment/Balloonscript.cs
+++ Assets/Scripts/Enviroment/Balloonscript.cs
@@ -16,6 +16,7 @@ public class Balloonscript : MonoBehaviour
 
     public float speed;
     bool balloondown;
+    bool isplayeron;
     
     void Start()
     {
@@ -25,30 +26,42 @@ public class Balloonscript : MonoBehaviour
     
     void Update()
     {
-        Startballoon();
-    }
-    void Startballoon()
-    {
-        if(Vector2.Distance(player.position, balloon.position) < 0.5f && Input.GetKeyDown("e"))
+        if (transform.position.y <= down.position.y)
         {
-            if(transform.position.y <= down.position.y)
-            {
-                balloondown = true;
-            }
-            else if(transform.position.y >= up.position.y)
-            {
-                balloondown = false;
-            }
+            balloondown = true;
         }
-
-        if (balloondown)
+        else if (transform.position.y >= up.position.y)
+        {
+            balloondown = false;
+        }
+        if (isplayeron)
         {
             transform.position = Vector2.MoveTowards(transform.position, up.position, speed * Time.deltaTime);
         }
-        else
+        else if (!isplayeron)
         {
             transform.position = Vector2.MoveTowards(transform.position, down.position, speed * Time.deltaTime);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            isplayeron = true;
+
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        isplayeron = false;
+    }
+
+
+
+
 
 }
