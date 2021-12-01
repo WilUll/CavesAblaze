475e7af7d084887821c0946f7dd2034658b0fa17
luisgvecchio <90183135+luisgvecchio@users.noreply.github.com>
2021-12-01 19:39:39 +0100

    Added little camera shake script ( applied in; Friday/test level). Changed Geyser back to how it was

---
+++ Assets/Scripts/Camera/ShakeCamera.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCamera : MonoBehaviour
+{
+    Camera mainCamera;
+
+    float shakeAmount = 0;
+
+    private void Awake()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            Shake(0.1f, 0.2f);
+        }
+    }
+
+    public void Shake (float amount, float lenght)
+    {
+        shakeAmount = amount;
+        InvokeRepeating("StartShake", 0, 0.1f);
+        Invoke("StopShake", lenght);
+    }
+
+    void StartShake()
+    {
+        if(shakeAmount > 0)
+        {
+            Vector3 cameraPosition = mainCamera.transform.position;
+
+            float offsetX = Random.value * shakeAmount * 2 - shakeAmount;
+            float offsetY = Random.value * shakeAmount * 2 - shakeAmount;
+
+            cameraPosition.x += offsetX;
+            cameraPosition.y += offsetY;
+
+            mainCamera.transform.position = cameraPosition;
+        }
+    }
+
+    void StopShake ()
+    {
+        CancelInvoke("StartShake");
+
+        mainCamera.transform.localPosition = Vector3.zero;
+    }
+}
