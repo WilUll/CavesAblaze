242fd85fd2a92e64bd9ae787970fd7cae3b5222a
WilUll <90192382+WilUll@users.noreply.github.com>
2021-12-02 14:34:25 +0100

    Text mesh pro added

--- Assets/flyScript.cs
+++ Assets/flyScript.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class flyScript : MonoBehaviour
 {
-    Rigidbody2D fly;
+    Text textOverFly;
+
     // Start is called before the first frame update
     void Start()
     {
-        fly = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        fly.velocity = (new Vector2(Random.Range(-4, 5), Random.Range(-4, 5)));
+        
     }
 }
