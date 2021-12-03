421c3910c941530bb99b7816b2385c0d4bb72943
WilUll <90192382+WilUll@users.noreply.github.com>
2021-12-03 09:15:15 +0100

    ADDED TEXT

--- Assets/flyScript.cs
+++ Assets/flyScript.cs
@@ -1,21 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class flyScript : MonoBehaviour
 {
-    Text textOverFly;
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Type());
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            NextSentence();
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
     }
 }
