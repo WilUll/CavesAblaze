2c56236839713b9e77c724ffc6a91cb82ba2fcb8
luisgvecchio <90183135+luisgvecchio@users.noreply.github.com>
2021-12-06 15:30:34 +0100

    Lantern works. Dash speed adjusted

--- Assets/LanternController.cs
+++ Assets/LanternController.cs
@@ -7,31 +7,44 @@ public class LanternController : MonoBehaviour
     
     
 
-    public float rotation1, rotation2, rotation3;
+    public float initialRotation, maxRotation;
 
-    float[] lanterPositions = new float[4];
+    float currentRotation;
 
-    int currentRotation, initialRotation;
+    public float inputMovementSpeed, goBackSpeed;
+
+    bool rotate;
 
     // Start is called before the first frame update
     void Start()
     {
-        lanterPositions[0] = initialRotation;
-        lanterPositions[1] = rotation1;
-        lanterPositions[2] = rotation2;
-        lanterPositions[3] = rotation3;
-
-        transform.rotation = Quaternion.Euler(0, 0, lanterPositions[currentRotation]);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
+        if (Input.GetKey(KeyCode.B))
         {
-            currentRotation++;
-
-            if (currentRotation > lanterPositions.Length) currentRotation = initialRotation;
-
-            transform.rotation = Quaternion.Euler(0, 0, lanterPositions[currentRotation]);
+            rotate = true;
         }
+        else
+        {
+            rotate = false;
+        }
+
+        if (rotate)
+        {
+            if (currentRotation > maxRotation) currentRotation -= inputMovementSpeed;
+            else currentRotation = initialRotation;
+        }
+        else
+        {
+            if (currentRotation > initialRotation) currentRotation = initialRotation;
+            if(currentRotation != initialRotation) currentRotation += goBackSpeed;
+        }
+
+        
+        transform.rotation = Quaternion.Euler(0, 0, currentRotation);
+        
+
     }
 }
