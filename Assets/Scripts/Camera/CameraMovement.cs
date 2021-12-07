ee0c2e66e1d0c8ea18e40dd1fb5860adb2573aa4
luisgvecchio <90183135+luisgvecchio@users.noreply.github.com>
2021-12-07 15:05:30 +0100

    Camera System ground created for the first level

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -42,22 +42,22 @@ public class CameraMovement : MonoBehaviour
         yDifference = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
 
         Vector3 newPosition = transform.position;
+        newPosition.x = Mathf.Clamp(transform.position.x, xMinClamp, xMaxClamp);
+        newPosition.y = Mathf.Clamp(transform.position.y, yMinClamp, yMaxClamp);
 
         if (Mathf.Abs(xDifference) >= threshold.x)
         {
-            newPosition.x = follow.x;
-            newPosition.x = Mathf.Clamp(newPosition.x, xMinClamp, xMaxClamp);
+            newPosition.x = Mathf.Clamp(follow.x, xMinClamp, xMaxClamp);
         }
         if (Mathf.Abs(yDifference) >= threshold.y) 
         {
-            newPosition.y = follow.y + yOffset;
-            newPosition.y = Mathf.Clamp(newPosition.y, yMinClamp, yMaxClamp);
+            newPosition.y = Mathf.Clamp(follow.y + yOffset, yMinClamp, yMaxClamp);
         }
         float moveSpeed = rb.velocity.magnitude > speed ? rb.velocity.magnitude : speed;
 
         transform.position = Vector3.Lerp(transform.position, newPosition, 5f * Time.deltaTime);
 
-        transform.LookAt(followObject.transform);
+        //transform.LookAt(followObject.transform);
         transform.rotation = Quaternion.identity;
 
         Camera.main.orthographicSize = ortographicSize;
