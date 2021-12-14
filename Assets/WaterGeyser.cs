3cbeebdbc5731f0208ad938fd63ad1a0562ce565
WilUll <90192382+WilUll@users.noreply.github.com>
2021-12-14 09:01:31 +0100

    william push

--- Assets/WaterGeyser.cs
+++ Assets/WaterGeyser.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class WaterGeyser : MonoBehaviour
 {
+    GameObject waterSprite;
+    Transform startPos, endPos;
+    float startSpeed, backSpeed;
+    bool isPlayerClose;
+    bool isShooting = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlayerClose && !isShooting)
+        {
+            
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isPlayerClose = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isPlayerClose = false;
+    }
+
+
 }
