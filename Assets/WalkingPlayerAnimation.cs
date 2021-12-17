79477e4ac3f545f770209075d8b76b580e18a9ef
WilUll <90192382+WilUll@users.noreply.github.com>
2021-12-17 11:56:50 +0100

    bv

--- Assets/WalkingPlayerAnimation.cs
+++ Assets/WalkingPlayerAnimation.cs
@@ -4,29 +4,20 @@ using UnityEngine;
 
 public class WalkingPlayerAnimation : MonoBehaviour
 {
-    float[] rotations = new float [7];
+    float[] rotations = {-20, -10, 0, 10, 20, 10, 0, -10};
 
     public float resetRotationTimer;
-    public Vector2 yOffsetMovement;
+    public Vector3 yOffsetMovement;
 
     float changeRotationTimer;
 
     int index = 0;
 
-
+    int loopTimes;
 
     void Start()
     {
         changeRotationTimer = resetRotationTimer;
-
-        rotations[0] = -20;
-        rotations[1] = -10;
-        rotations[2] = 0;
-        rotations[3] = 10;
-        rotations[4] = 20;
-        rotations[5] = 10;
-        rotations[6] = 0;
-        rotations[7] = -10;
     }
 
 
@@ -36,21 +27,26 @@ public class WalkingPlayerAnimation : MonoBehaviour
         {
             index++;
 
-            if (index > 7)
+            if (index >= rotations.Length)
             {
                 index = 0;
             }
 
             transform.rotation = Quaternion.Euler(0, 0, rotations[index]);
-            transform.localPosition = yOffsetMovement;
+            transform.localPosition += (yOffsetMovement);
 
             changeRotationTimer = resetRotationTimer;
+
+            loopTimes++;
+            if (loopTimes == 3)
+            {
+                yOffsetMovement = -yOffsetMovement;
+                loopTimes = 0;
+            }
         }
         else
         {
-            transform.localPosition = Vector2.zero;
             changeRotationTimer -= Time.deltaTime;
         }
-
     }
 }
