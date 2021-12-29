fb83c2d1c5cd76f46ed478f2c75a18a17aa72f9e
WilUll <90192382+WilUll@users.noreply.github.com>
2021-12-29 15:43:07 +0100

    ffgdfg

---
+++ Assets/DontDestroyManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyManager : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
