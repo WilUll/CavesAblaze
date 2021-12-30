6f4f003cab6608949d08e38da64c0b003574a551
luisgvecchio <90183135+luisgvecchio@users.noreply.github.com>
2021-12-30 10:15:40 +0100

    Added light whrn not on fire (Bonfire)

--- Assets/BonfireBaseLightManager.cs
+++ Assets/BonfireBaseLightManager.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class BonfireBaseLightManager : MonoBehaviour
 {
     public FireManager fireManagerScript;
-    void Start()
-    {
-        
-    }
+    
     void Update()
     {
         if(fireManagerScript.baseLight)
         {
-            gameObject.SetActive(true);
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            gameObject.transform.GetChild(2).gameObject.SetActive(true);
+            gameObject.transform.GetChild(3).gameObject.SetActive(true);
+            gameObject.transform.GetChild(4).gameObject.SetActive(true);
         }
         else
         {
-            gameObject.SetActive(false);
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+            gameObject.transform.GetChild(2).gameObject.SetActive(false);
+            gameObject.transform.GetChild(3).gameObject.SetActive(false);
+            gameObject.transform.GetChild(4).gameObject.SetActive(false);
         }
     }
 }
--- Assets/Scripts/Enviroment/FireManager.cs
+++ Assets/Scripts/Enviroment/FireManager.cs
@@ -15,21 +15,20 @@ public class FireManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!checkpointScript.isBurning)
+        if (!checkpointScript.isBurning)
         {
-            foreach (Transform child in transform)
-            {
-                child.gameObject.SetActive(false);
-                baseLight = true;
-            }
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+            gameObject.transform.GetChild(2).gameObject.SetActive(false);
+            gameObject.transform.GetChild(3).gameObject.SetActive(false);
+            gameObject.transform.GetChild(4).gameObject.SetActive(false);
         }
         else
         {
-            foreach (Transform child in transform)
-            {
-                child.gameObject.SetActive(true);
-                baseLight = false;
-            }
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            gameObject.transform.GetChild(2).gameObject.SetActive(true);
+            gameObject.transform.GetChild(3).gameObject.SetActive(true);
+            gameObject.transform.GetChild(4).gameObject.SetActive(true); 
         }
     }
 }
