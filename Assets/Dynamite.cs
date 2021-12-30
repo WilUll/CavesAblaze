6b5c75b276cc5127aba537f87b59f9f5bd56fb80
WilUll <90192382+WilUll@users.noreply.github.com>
2021-12-30 14:22:20 +0100

    bbb

--- Assets/Dynamite.cs
+++ Assets/Dynamite.cs
@@ -34,18 +34,19 @@ public class Dynamite : MonoBehaviour
             if (Vector2.Distance(fusePos[fusePos.Length - 1].position, fusePos[0].position) > 1f)
             {
                 startTime += Time.deltaTime;
-                float leftToGo = (startTime / 10);
-                Debug.Log(leftToGo);
+                float leftToGo = (startTime / 20);
                 LR.SetPosition(index, Vector2.Lerp(fusePos[index].position, fusePos[index - 1].position, leftToGo));
+                if (index == fusePos.Length - 2)
+                {
+                    LR.SetPosition(index + 1, LR.GetPosition(index));
+                    fusePos[index + 1].position = LR.GetPosition(index);
+
+                }
                 fusePos[index].position = LR.GetPosition(index);
-                if (leftToGo >= 1 || Vector2.Distance(fusePos[index].position, fusePos[index - 1].position) <= 0.1f)
+                if (leftToGo >= 1 || Vector2.Distance(fusePos[index].position, fusePos[index - 1].position) <= 0.2f)
                 {
                     startTime = 0;
                     index--;
-                    if (index == 0)
-                    {
-                        index = fusePos.Length - 1;
-                    }
                 }
 
             }
@@ -57,7 +58,7 @@ public class Dynamite : MonoBehaviour
                     isIgnited = false;
                     explosion.Play();
                     yield return new WaitForSeconds(0.2f);
-                    transform.parent.gameObject.GetComponent<ExplodingWall>().DamageWall();
+                    transform.parent.gameObject.GetComponent<ExplodingWall>().DamageWall(transform);
                     gameObject.SetActive(false);
                 }
 
--- Assets/ExplodingWall.cs
+++ Assets/ExplodingWall.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class ExplodingWall : MonoBehaviour
 {
     public int health;
-
-    public void DamageWall()
+    GameObject shatter;
+    ParticleSystem shatterSystem;
+    private void Start()
+    {
+        shatter = gameObject.transform.Find("ParticlesShatter").gameObject;
+        shatterSystem = shatter.GetComponent<ParticleSystem>();
+    }
+    public void DamageWall(Transform positionSystem)
     {
         health--;
+        shatter.transform.position = new Vector3(positionSystem.position.x - 0.5f, positionSystem.position.y, positionSystem.position.z);
+        shatterSystem.Play();
         if (health == 0)
         {
             gameObject.SetActive(false);
