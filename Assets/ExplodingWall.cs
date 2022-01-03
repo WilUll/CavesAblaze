0202ffe9853998c73890ca8e9c72d31421d5ca94
luisgvecchio <90183135+luisgvecchio@users.noreply.github.com>
2022-01-03 11:31:21 +0100

    Fixed dinamyte on level 2

--- Assets/ExplodingWall.cs
+++ Assets/ExplodingWall.cs
@@ -5,21 +5,49 @@ using UnityEngine;
 public class ExplodingWall : MonoBehaviour
 {
     public int health;
-    GameObject shatter;
+    public GameObject shatter;
     ParticleSystem shatterSystem;
+    public SpriteRenderer wallSprite;
+    public Sprite[] spriteArray;
+    public AudioSource audioSource;
+
+    BoxCollider2D boxCollider;
+    PolygonCollider2D polygonCollider;
+
     private void Start()
     {
-        shatter = gameObject.transform.Find("ParticlesShatter").gameObject;
+        boxCollider = GetComponent<BoxCollider2D>();
+        polygonCollider = GetComponent<PolygonCollider2D>();
         shatterSystem = shatter.GetComponent<ParticleSystem>();
+        audioSource = GetComponent<AudioSource>();
     }
+
     public void DamageWall(Transform positionSystem)
     {
         health--;
         shatter.transform.position = new Vector3(positionSystem.position.x - 0.5f, positionSystem.position.y, positionSystem.position.z);
         shatterSystem.Play();
-        if (health == 0)
+
+        switch (health)
         {
-            gameObject.SetActive(false);
+            case 0:
+                wallSprite.sprite = spriteArray[2];
+                boxCollider.enabled = false;
+                polygonCollider.enabled = false;
+                break;
+            case 1:
+                wallSprite.sprite = spriteArray[1];
+                boxCollider.enabled = false;
+                polygonCollider.enabled = true;
+                Debug.Log("1");
+                break;
+            case 2:
+                wallSprite.sprite = spriteArray[0];
+                boxCollider.enabled = true;
+                polygonCollider.enabled = false;
+
+                break;
+
         }
     }
 }
