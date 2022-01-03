4cec9c26a65a2b93ce3b929d19c2066f5c5bb38d
WilUll <90192382+WilUll@users.noreply.github.com>
2022-01-03 11:39:31 +0100

    f

---
+++ Assets/TutorialText.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TutorialText : MonoBehaviour
+{
+    TextMeshProUGUI tutText;
+    public string[] sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            tutText.text = "";
+            for (int i = 0; i < triggerGameObject.Length; i++)
+            {
+                tutScript = triggerGameObject[i].GetComponent<TutorialTriggers>();
+            }
+        }
+    }
+}
