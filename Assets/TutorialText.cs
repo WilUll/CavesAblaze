a8d47ce5a0118d8ca90345ac99cf4d8927e847a4
WilUll <90192382+WilUll@users.noreply.github.com>
2022-01-03 11:56:47 +0100

    fddgfgfd

--- Assets/TutorialText.cs
+++ Assets/TutorialText.cs
@@ -7,11 +7,15 @@ public class TutorialText : MonoBehaviour
 {
     TextMeshProUGUI tutText;
     public string[] sentences;
+    int index;
 
     // Start is called before the first frame update
     void Start()
     {
         tutText = GetComponent<TextMeshProUGUI>();
+        tutText.text = sentences[index].ToString();
+        tutText.CrossFadeAlpha(1f, 0f, false);
+        Debug.Log(Input.GetJoystickNames());
     }
 
     // Update is called once per frame
@@ -19,11 +23,24 @@ public class TutorialText : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.G))
         {
-            tutText.text = "";
-            for (int i = 0; i < triggerGameObject.Length; i++)
-            {
-                tutScript = triggerGameObject[i].GetComponent<TutorialTriggers>();
-            }
+            StartCoroutine(fade());
+        }
+    }
+
+    IEnumerator fade()
+    {
+        tutText.CrossFadeAlpha(0f, 0.2f, false);
+        yield return new WaitForSeconds(0.2f);
+        tutText.text = "";
+        index++;
+        if (index > sentences.Length)
+        {
+
+        }
+        else
+        {
+            tutText.text = sentences[index].ToString();
+            tutText.CrossFadeAlpha(1f, 0.2f, false);
         }
     }
 }
