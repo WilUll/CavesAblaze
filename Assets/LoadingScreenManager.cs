95b28dea6f17cadd5ae9ea890586f2dc95ab2575
WilUll <90192382+WilUll@users.noreply.github.com>
2022-01-06 17:25:25 +0100

    vcvv

--- Assets/LoadingScreenManager.cs
+++ Assets/LoadingScreenManager.cs
@@ -6,9 +6,12 @@ using TMPro;
 public class LoadingScreenManager : MonoBehaviour
 {
     public TextMeshProUGUI time, score, deaths;
+    float finalScore, x;
+    bool added,stop = true;
 
     private void Start()
     {
+        x = 0;
         deaths.text = SceneScript.Instance.playerDeaths.ToString();
         string minutes = Mathf.Floor(SceneScript.Instance.timer / 60).ToString("00");
         string seconds = (SceneScript.Instance.timer % 60).ToString("00");
@@ -18,12 +21,40 @@ public class LoadingScreenManager : MonoBehaviour
         if (SceneScript.Instance.playerDeaths == 0)
         {
             int scoreInt = (int)((1000 / (SceneScript.Instance.timer))* 200);
-            score.text = scoreInt.ToString();
+            scoreInt = (Mathf.RoundToInt(scoreInt / 100)) * 100;
+            finalScore = scoreInt;
+            stop = false;
         }
         else
         {
             int scoreInt = (int)((1000 / (SceneScript.Instance.timer * SceneScript.Instance.playerDeaths))*100);
-            score.text = scoreInt.ToString();
+            scoreInt = (Mathf.RoundToInt(scoreInt / 100)) * 100;
+            finalScore = scoreInt;
+            stop = false;
         }
+        if (added)
+        {
+
+        }
+        else
+        {
+            SceneScript.Instance.totalScore += finalScore;
+            added = true;
+        }
+
+    }
+    private void Update()
+    {
+        if (!stop)
+        {
+            x +=(finalScore - x) * 0.005f;
+            x = Mathf.RoundToInt(x);
+        }
+        else if (x + 1 >= finalScore)
+        {
+            stop = true;
+            x = finalScore;
+        }
+        score.text = x.ToString();
     }
 }
