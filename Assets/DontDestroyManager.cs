dfb49aedfd0a3279b78d15ab08207dac6d225986
luisgvecchio <90183135+luisgvecchio@users.noreply.github.com>
2022-01-07 16:19:51 +0100

    EndScene with music now

--- Assets/DontDestroyManager.cs
+++ Assets/DontDestroyManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroyManager : MonoBehaviour
 {
+    AudioSource audioSource;
+    public AudioClip [] clip;
+
+
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         DontDestroyOnLoad(this.gameObject);
-    }
 
-    void Update()
-    {
-        
+        if(SceneManager.GetActiveScene().name == "End scen")
+        {
+            audioSource.clip = clip[0];
+            audioSource.Play();
+        }
     }
 }
