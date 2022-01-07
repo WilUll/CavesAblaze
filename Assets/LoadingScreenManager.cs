43c9f2a007d8b51318f8c0f7aa98c236179130a7
WilUll <90192382+WilUll@users.noreply.github.com>
2022-01-07 12:10:48 +0100

    mrgrtrrererererer

--- Assets/LoadingScreenManager.cs
+++ Assets/LoadingScreenManager.cs
@@ -6,12 +6,13 @@ using TMPro;
 public class LoadingScreenManager : MonoBehaviour
 {
     public TextMeshProUGUI time, score, deaths;
-    float finalScore, x;
+    float finalScore, x, timer;
     public bool added,stop = true;
 
     private void Start()
     {
         x = 0;
+        score.text = 0.ToString();
         deaths.text = SceneScript.Instance.playerDeaths.ToString();
         string minutes = Mathf.Floor(SceneScript.Instance.timer / 60).ToString("00");
         string seconds = Mathf.FloorToInt((SceneScript.Instance.timer % 60)).ToString();
@@ -23,26 +24,32 @@ public class LoadingScreenManager : MonoBehaviour
             int scoreInt = (int)((1000 / (SceneScript.Instance.timer))* 200);
             scoreInt = (Mathf.RoundToInt(scoreInt / 100)) * 100;
             finalScore = scoreInt;
-            stop = false;
+            stop = true;
         }
         else
         {
             int scoreInt = (int)((1000 / (SceneScript.Instance.timer * SceneScript.Instance.playerDeaths))*100);
             scoreInt = (Mathf.RoundToInt(scoreInt / 100)) * 100;
             finalScore = scoreInt;
-            stop = false;
+            stop = true;
         }
         
 
     }
     private void Update()
     {
+        timer += Time.deltaTime;
+        if (timer >= 0.25f)
+        {
+            stop = false;
+        }
         if (!stop)
         {
-            x +=(finalScore - x) * 0.005f;
+            x +=(finalScore - x) * 0.01f;
             x = Mathf.RoundToInt(x);
+            score.text = x.ToString();
         }
-        if (x + 100 >= finalScore && !stop)
+        if (x + 250 >= finalScore && !stop)
         {
             stop = true;
             x = finalScore;
@@ -55,8 +62,8 @@ public class LoadingScreenManager : MonoBehaviour
                 SceneScript.Instance.totalScore += finalScore;
                 added = true;
             }
+            score.text = x.ToString();
         }
-        score.text = x.ToString();
 
     }
 }
